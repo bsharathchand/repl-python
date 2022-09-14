2abc61d842a686df9229df0426b9e945114bf8e4
Sharath Bhaskara <bsharathchand@users.noreply.github.com>
2022-09-14 02:33:42 +0000

    fruit-basket program

--- src/fill2022/practice_tests/fruit-basket.py
+++ src/fill2022/practice_tests/fruit-basket.py
@@ -1,22 +1,8 @@
-from src.utilities.utility import objectutils 
-
-utl = objectutils()
-
-_add = utl.add(3,5)
-
-_sub = utl.sub(5, 2)
-
-print(_add)
-print(_sub)
-
-print(utl.sub(2,5))
-
-# A collection of type list can contain many occurances of the same element
+# Collection of fruits in a basket as a list
 basket = ['mango','apple','plum','peach','mango','mango','peach','apple','peach','plum']
 
 
 # Mango weighs 100 fl oz, Apple weighs 30 fl oz, plum weighs 25 fl oz and peach weigh 15 fl oz. 
-# A dictionary in python holds key value pairs
 fruitWeightMap = {'mango':100, 'apple': 30, 'peach': 15, 'plum': 25}
 
 weightOfBasket = 0
